ebfb1dd31a863292a540a957623281499ddd0c12
zhangwei <zhangwei@belink.com>
2023-04-07 10:19:45 +0800

    初始化项目

--- .history/src/App_20230406201825.vue
+++ .history/src/App_20230406201825.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="这是一个桌面端应用"/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
